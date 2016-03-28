cdb8b8d33fa547e6fc7596b1b680326494659345
Leonardo Picoli <Leonardo Picoli>
2016-03-28 09:22:20 -0300

    Painting with names

--- app/src/main/java/com/example/lgpicoli/brainy/BrainyActivity.java
+++ app/src/main/java/com/example/lgpicoli/brainy/BrainyActivity.java
@@ -1,6 +1,7 @@
 package com.example.lgpicoli.brainy;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -8,6 +9,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
+import android.graphics.Typeface;
 import android.view.Display;
 import android.view.View;
 import android.view.WindowManager;
@@ -41,20 +43,39 @@ public class BrainyActivity extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        Typeface face = Typeface.create("self-sarian", Typeface.BOLD);
+
+
         Paint blackRectPaintBrush = new Paint();
         blackRectPaintBrush.setColor(Color.BLACK);
         blackRectPaintBrush.setStyle(Paint.Style.FILL);
+
+        Paint textBigPaintbrush = new Paint();
+        textBigPaintbrush.setColor(Color.WHITE);
+        textBigPaintbrush.setTextAlign(Paint.Align.CENTER);
+        textBigPaintbrush.setTextSize(18);
+        textBigPaintbrush.setTypeface(Typeface.MONOSPACE);
+
+        Paint textPaintbrush = new Paint();
+        textPaintbrush.setColor(Color.WHITE);
+        textPaintbrush.setTextAlign(Paint.Align.CENTER);
+        textPaintbrush.setTextSize(12);
+        textPaintbrush.setTypeface(Typeface.MONOSPACE);
 
         Rect rect = new Rect();
         rect.set(0, 0, this.screenWidth, this.screenHeight);
 
         canvas.drawRect(rect, blackRectPaintBrush);
 
-        Bitmap sourceBm = BitmapFactory.decodeResource(getResources(), R.drawable.brain5);
-        drawBitmap(canvas, sourceBm, 50, screenHeight/2);
-
         Bitmap sourceBm2 = BitmapFactory.decodeResource(getResources(), R.drawable.brain12);
-        drawBitmap(canvas, sourceBm2, screenWidth/2, screenHeight/2);
+        canvas.drawText("Textoo Bola Pequena", screenWidth / 2, screenHeight / 2 + sourceBm2.getHeight() / 2 + 2 * 18, textBigPaintbrush);
+        drawBitmap(canvas, sourceBm2, screenWidth / 2, screenHeight / 2);
+
+        Bitmap sourceBm = BitmapFactory.decodeResource(getResources(), R.drawable.brain5);
+        int dis = screenWidth/2 - sourceBm2.getWidth()/2;
+        int xPos = dis/2;
+        drawBitmap(canvas, sourceBm, xPos, screenHeight / 2);
+        canvas.drawText("Textoo Bola Pequena", xPos, screenHeight / 2 + sourceBm.getHeight() / 2 + 2 * 12, textPaintbrush);
     }
 
     private void drawBitmap(Canvas canvas, Bitmap bitmap, int x, int y) {
---
+++ app/src/main/java/com/example/lgpicoli/brainy/DateUnity.java
@@ -0,0 +1,8 @@
+package com.example.lgpicoli.brainy;
+
+/**
+ * Created by Leonardo on 26/03/2016.
+ */
+public enum DateUnity {
+    MINUTE, HOUR, DAY, MONTH, YEAR, WEEK
+}
